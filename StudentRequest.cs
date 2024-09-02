46fe24fe4b791a46df4d8ae65ce2e3f30906ba8a
Ankitadhkad <161940018+Ankitadhkad@users.noreply.github.com>
2024-09-02 12:00:43 +0530

    E:\Ankita\CrudUsingReact\CrudUsingReact.Server\Models

---
+++ StudentRequest.cs
@@ -0,0 +1,12 @@
+﻿namespace CrudUsingReact.Server.ViewModels
+{
+    public class StudentRequest
+    {
+        public string Name { get; set; }
+        public string Rollno { get; set; }
+        public string Class { get; set; }
+        public string Address { get; set; }
+        public string Gender { get; set; }
+
+    }
+}
